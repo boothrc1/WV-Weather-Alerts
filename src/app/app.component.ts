c9f667809e0da1f47a681d83fb20372a5186339a
Ryan Booh <macuser@Macs-MacBook-Pro-2.local>
2026-01-25 14:23:01 -0500

    Initial commit: Weather alerts UI

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, Subject } from 'rxjs';
+import { map } from 'rxjs/operators'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit, OnDestroy {
+  title = 'd3-experiment';
+  alerts: [];
+
+  private destroy$ = new Subject<void>();
+
+  private apiUrl = 'https://api.weather.gov/alerts/active?area=WV'; 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getActiveAlerts().subscribe(data => this.alerts = data);
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+  
+  getActiveAlerts(): Observable<any> { 
+    return this.http.get<any>(this.apiUrl).pipe(
+      map(response => {
+        return response.features.map(f => ({ 
+          id: f.properties.id,
+          event: f.properties.event,
+          headline: f.properties.headline, 
+          description: f.properties.description,
+          severity: f.properties.severity,
+          certainty: f.properties.certainty,
+          urgency: f.properties.urgency,
+          areas: f.properties.areaDesc.split(';').map(a => a.trim()), sent: f.properties.sent, expires: f.properties.expires })); 
+        }));
+      };
+}
